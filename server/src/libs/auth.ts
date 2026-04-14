ef1512f960bca49657e584e5f01a9a7a326805cb
Tanmay Islam <tanmayislam101@gmail.com>
2026-04-14 17:31:11 +0600

    feat: implement authentication system using Better Auth with Prisma schema and migrations

---
+++ server/src/libs/auth.ts
@@ -0,0 +1,90 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { bearer } from "better-auth/plugins";
+import { Role, UserStatus } from "src/generated/prisma";
+
+
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false
+  },
+
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: Role.USER,
+      },
+
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: UserStatus.ACTIVE,
+      },
+
+      isDeleted: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+
+      deletedAt: {
+        type: "date",
+        required: false,
+        defaultValue: null,
+      },
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    sendOnSignIn: true,
+    autoSignInAfterVerification: true,
+  },
+  plugins:[bearer()],
+  session: {
+    // better-auth expects seconds here (not milliseconds)
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    },
+  },
+
+  trustedOrigins: [process.env.BETTER_AUTH_URL || "http://localhost:5000"],
+
+  advanced: {
+    // disableCSRFCheck: true,
+    useSecureCookies: false,
+    cookies: {
+      state: {
+        attributes: {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+          path: "/",
+        },
+      },
+      sessionToken: {
+        attributes: {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+          path: "/",
+        },
+      },
+    },
+  },
+});
